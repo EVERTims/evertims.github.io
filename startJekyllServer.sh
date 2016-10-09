3d9d5fc6706f1315afdd975cb0c8c6640de79774
David Poirier-Quinot <davipoir@gmail.com>
2016-10-09 14:45:27 +0200

    added bash script to launch jekyll on localhost (debug)

---
+++ startJekyllServer.sh
@@ -0,0 +1,12 @@
+# for Bundler setup, see: 
+# https://help.github.com/articles/setting-up-your-github-pages-site-locally-with-jekyll/
+# 
+# basically (if ruby installed):
+# > gem install bundler
+# > bundle install
+
+# launch localhost http server
+bundle exec jekyll serve --baseurl ''
+
+# open page in browser
+# open /Applications/Browsers/Google\ Chrome.app/ http://localhost:4000/
